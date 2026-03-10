c8acfbe87f072e813cf7ee2c80a8931c615db5f1
rekuskz-gif <rekuskz@gmail.com>
2026-03-10 08:12:36 +0300

    Update quiz.js

--- api/quiz.js
+++ api/quiz.js
@@ -1,12 +1,27 @@
 export default async function handler(req, res) {
 
 if (req.method !== "POST") {
-return res.status(405).end()
+return res.status(405).json({error:"Method not allowed"})
 }
 
 const data = req.body
 
-console.log("Новый квиз:", data)
+try {
+
+await fetch("https://api.telegram.org/bot8715209750:AAH4-blEgXPZpeYXii8IeWLX0wdbGWtANQc/sendMessage",{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body:JSON.stringify({
+chat_id:"376719975",
+text:"Новый квиз:\n"+JSON.stringify(data,null,2)
+})
+})
+
+} catch(e) {
+console.log(e)
+}
 
 res.status(200).json({ok:true})
 
